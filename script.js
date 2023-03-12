299c73d4ef4138ce47051712bc61387127928bae
Daniel <daniel@MacBook-Pro.local>
2023-03-13 00:15:00 +1100

    Add theme switcher and project gifs

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const textArray = ["HTML", "CSS", "JAVASCRIPT", "REACTJS", "ANGULAR", "NODEJS", ".NET"];
+// Typewriter effect fun
+const textArray = [
+  "HTML",
+  "CSS",
+  "JAVASCRIPT",
+  "REACTJS",
+  "ANGULAR",
+  "NODEJS",
+  ".NET",
+  "Motorcycling!",
+];
 const codeBox = document.getElementById("code-wrapper");
 
+// Recursively write letters in line until end of sentence
 const writeLine = (text) =>
   new Promise((resolve) => {
     const el = document.createElement("p");
-    // el.innerHTML = "> ";
     el.classList.add("active");
     codeBox.appendChild(el);
 
@@ -21,6 +31,7 @@ const writeLine = (text) =>
     write(0);
   });
 
+// Recursively write lines until end of array
 const writeLines = (lines) => {
   lines.length &&
     writeLine(lines[0]).then(() => {
@@ -30,3 +41,32 @@ const writeLines = (lines) => {
 };
 
 writeLines(textArray);
+
+// Light/dark theme shenanigans
+const checkbox = document.getElementById("switch");
+
+if (localStorage.getItem("theme") === "dark") {
+  checkbox.checked = true;
+  document.body.classList.toggle("dark");
+}
+
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+  console.log(checkbox.checked);
+  localStorage.setItem("theme", checkbox.checked ? "dark" : "light");
+});
+
+// Project wrapper click listeners
+document
+  .getElementById("driftjs")
+  .addEventListener("click", () => window.open("https://driftjs.io", "_blank"));
+
+document
+  .getElementById("notish")
+  .addEventListener("click", () => window.open("https://notish.io", "_blank"));
+
+document
+  .getElementById("deciball")
+  .addEventListener("click", () =>
+    window.open("https://deciball.io", "_blank")
+  );
